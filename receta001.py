411b41cfe320fba65c6f017df4d37dbf9bdf6833
Yokekomo <albanochicharroaltur@gmail.com>
2022-01-14 20:09:37 +0100

    Receta Python

---
+++ receta001.py
@@ -0,0 +1,14 @@
+# Receta Python 001: Crear variables independientes para cada elemento de una lista o tupla.
+
+
+""" Crear variables independientes para una tupla """
+
+tupla = (1, 3)
+a, b = tupla
+print(a, b)
+
+""" Crear variables independientes para una lista """
+
+lista = ['Python', 3, (4, 5), 'P']
+a, b, c, d = lista
+print(a, b, c, d)
